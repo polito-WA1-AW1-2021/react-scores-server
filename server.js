99c286269cbf2fa119f583b942c13b64ab239f2f
Luigi De Russis <luigidr@gmail.com>
2021-05-27 13:36:33 +0200

    add authentication

--- server.js
+++ server.js
@@ -3,7 +3,41 @@
 const express = require('express');
 const morgan = require('morgan'); // logging middleware
 const {check, validationResult} = require('express-validator'); // validation middleware
-const dao = require('./dao'); // module for accessing the DB
+const examDao = require('./exam-dao'); // module for accessing the exams in the DB
+const passport = require('passport'); // auth middleware
+const LocalStrategy = require('passport-local').Strategy; // username and password for login
+const session = require('express-session'); // enable sessions
+const userDao = require('./user-dao'); // module for accessing the users in the DB
+
+/*** Set up Passport ***/
+// set up the "username and password" login strategy
+// by setting a function to verify username and password
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    userDao.getUser(username, password).then((user) => {
+      if (!user)
+        return done(null, false, { message: 'Incorrect username and/or password.' });
+        
+      return done(null, user);
+    })
+  }
+));
+
+// serialize and de-serialize the user (user object <-> session)
+// we serialize the user id and we store it in the session: the session is very small in this way
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+// starting from the data in the session, we extract the current (logged-in) user
+passport.deserializeUser((id, done) => {
+  userDao.getUserById(id)
+    .then(user => {
+      done(null, user); // this will be available in req.user
+    }).catch(err => {
+      done(err, null);
+    });
+});
 
 // init express
 const app = express();
@@ -13,12 +47,32 @@ const port = 3001;
 app.use(morgan('dev'));
 app.use(express.json());
 
+// custom middleware: check if a given request is coming from an authenticated user
+const isLoggedIn = (req, res, next) => {
+  if(req.isAuthenticated())
+    return next();
+  
+  return res.status(401).json({ error: 'not authenticated'});
+}
 
-/*** APIs ***/
+// set up the session
+app.use(session({
+  // by default, Passport uses a MemoryStore to keep track of the sessions
+  secret: 'a secret sentence not to share with anybody and anywhere, used to sign the session ID cookie',
+  resave: false,
+  saveUninitialized: false 
+}));
+
+// then, init passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+/*** Courses/Exams APIs ***/
 
 // GET /api/courses
 app.get('/api/courses', (req, res) => {
-  dao.listCourses()
+  examDao.listCourses()
     .then(courses => res.json(courses))
     .catch(() => res.status(500).end());
 });
@@ -26,7 +80,7 @@ app.get('/api/courses', (req, res) => {
 // GET /api/courses/<code>
 app.get('/api/courses/:code', async (req, res) => {
   try {
-    const result = await dao.getCourse(req.params.code);
+    const result = await examDao.getCourse(req.params.code);
     if(result.error)
       res.status(404).json(result);
     else
@@ -37,9 +91,9 @@ app.get('/api/courses/:code', async (req, res) => {
 });
 
 // GET /api/exams
-app.get('/api/exams', async (req, res) => {
+app.get('/api/exams', isLoggedIn, async (req, res) => {
   try {
-    const exams = await dao.listExams();
+    const exams = await examDao.listExams(req.user.id);
     res.json(exams);
   } catch(err) {
     res.status(500).end();
@@ -47,7 +101,7 @@ app.get('/api/exams', async (req, res) => {
 });
 
 // POST /api/exams
-app.post('/api/exams', [
+app.post('/api/exams', isLoggedIn, [
   check('score').isInt({min: 18, max: 31}),
   check('code').isLength({min: 7, max: 7}),
   check('date').isDate({format: 'YYYY-MM-DD', strictMode: true})
@@ -64,7 +118,7 @@ app.post('/api/exams', [
   };
 
   try {
-    await dao.createExam(exam);
+    await examDao.createExam(exam, req.user.id);
     res.status(201).end();
   } catch(err) {
     res.status(503).json({error: `Database error during the creation of exam ${exam.code}.`});
@@ -72,7 +126,7 @@ app.post('/api/exams', [
 });
 
 // PUT /api/exams/<code>
-app.put('/api/exams/:code', [
+app.put('/api/exams/:code', isLoggedIn, [
   check('score').isInt({min: 18, max: 31}),
   check('code').isLength({min: 7, max: 7}),
   check('date').isDate({format: 'YYYY-MM-DD', strictMode: true})
@@ -86,7 +140,7 @@ app.put('/api/exams/:code', [
 
   // you can also check here if the code passed in the URL matches with the code in req.body
   try {
-    await dao.updateExam(exam);
+    await examDao.updateExam(exam, req.user.id);
     res.status(200).end();
   } catch(err) {
     res.status(503).json({error: `Database error during the update of exam ${req.params.code}.`});
@@ -95,15 +149,63 @@ app.put('/api/exams/:code', [
 });
 
 // DELETE /api/exams/<code>
-app.delete('/api/exams/:code', async (req, res) => {
+app.delete('/api/exams/:code', isLoggedIn, async (req, res) => {
   try {
-    await dao.deleteExam(req.params.code);
+    await examDao.deleteExam(req.params.code, req.user.id);
     res.status(204).end();
   } catch(err) {
     res.status(503).json({ error: `Database error during the deletion of exam ${req.params.code}.`});
   }
 });
 
+/*** Users APIs ***/
+
+// POST /sessions 
+// login
+app.post('/api/sessions', function(req, res, next) {
+  passport.authenticate('local', (err, user, info) => {
+    if (err)
+      return next(err);
+      if (!user) {
+        // display wrong login messages
+        return res.status(401).json(info);
+      }
+      // success, perform the login
+      req.login(user, (err) => {
+        if (err)
+          return next(err);
+        
+        // req.user contains the authenticated user, we send all the user info back
+        // this is coming from userDao.getUser()
+        return res.json(req.user);
+      });
+  })(req, res, next);
+});
+
+// ALTERNATIVE: if we are not interested in sending error messages...
+/*
+app.post('/api/sessions', passport.authenticate('local'), (req,res) => {
+  // If this function gets called, authentication was successful.
+  // `req.user` contains the authenticated user.
+  res.json(req.user);
+});
+*/
+
+// DELETE /sessions/current 
+// logout
+app.delete('/api/sessions/current', (req, res) => {
+  req.logout();
+  res.end();
+});
+
+// GET /sessions/current
+// check whether the user is logged in or not
+app.get('/api/sessions/current', (req, res) => {
+  if(req.isAuthenticated()) {
+    res.status(200).json(req.user);}
+  else
+    res.status(401).json({error: 'Unauthenticated user!'});;
+});
 
 /*** Other express-related instructions ***/
 
